ac8f22911125ecddb703aef279ae6660d1a9ada8
edemskiy <qapple@ya.ru>
2019-11-18 13:51:03 +0300

    Change popup window stylization

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -7,15 +7,10 @@ class Popup extends Component {
   render() {
     const { startNewGame, ...rest } = this.props;
     return (
-      <Modal
-        {...rest}
-        // size="md"
-        aria-labelledby="popup-window"
-        centered
-      >
+      <Modal {...rest} aria-labelledby="popup-window" centered>
         <div className="popup-window">
           <h3>the game is over ¯\_(ツ)_/¯</h3>
-          <h1 className="display-4">Your score: {this.props.score}</h1>
+          <h1 className>Your score: {this.props.score}</h1>
 
           <div className="popup-footer">
             <button
